9eb3e097460debcce8d7a5f7f71645012fa78723
Andrew Gorbokon <andrew.gorbokon@gmail.com>
2019-02-15 00:59:57 +0200

    Do not show datagrid for simple keys

--- Converters/NullCollapsedConverter.cs
+++ Converters/NullCollapsedConverter.cs
@@ -5,11 +5,11 @@ using System.Windows.Data;
 
 namespace Quickr.Converters
 {
-    internal class NullVisibilityConverter : IValueConverter
+    internal class NullCollapsedConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value == null ? Visibility.Hidden : Visibility.Visible;
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
