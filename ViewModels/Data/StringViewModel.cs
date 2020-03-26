71efb716202a0491abf6176c95ed26b5f7980cbb
Andrii Horbokon <andrii.horbokon@gmail.com>
2020-03-26 22:54:50 +0200

    string edit

--- ViewModels/Data/StringViewModel.cs
+++ ViewModels/Data/StringViewModel.cs
@@ -10,11 +10,13 @@ namespace Quickr.ViewModels.Data
         public StringViewModel(RedisProxy proxy, KeyEntry key): base(proxy, key)
         {
             Value = new ValueViewModel(Proxy.GetString(Key));
+            Value.ValueSaved += OnValueSaved;
         }
 
         protected void OnValueSaved(object sender, EventArgs e)
         {
             Proxy.SetString(Key, Value.CurrentValue);
+            Value.OriginalValue = Value.CurrentValue;
         }
     }
 }
