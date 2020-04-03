ea114f3d4c83fc24eda154a3a38d4af160ff4b9f
Andrii Horbokon <andrii.horbokon@gmail.com>
2020-04-04 01:19:47 +0300

    Property page in separate control

---
+++ Views/Configuration/NetworkPropertyPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quickr.Views.Configuration
+{
+    /// <summary>
+    /// Interaction logic for NetworkPropertyPage.xaml
+    /// </summary>
+    public partial class NetworkPropertyPage : UserControl
+    {
+        public NetworkPropertyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
