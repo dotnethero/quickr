fa054dac5f7adf83e7bb73cf04ad1896a856b5b3
Andrii Horbokon <andrii.horbokon@gmail.com>
2020-04-03 17:56:14 +0300

    Property pages base class

---
+++ ViewModels/Configuration/BasePropertyPageModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Quickr.Models.Configuration;
+using Quickr.Services;
+
+namespace Quickr.ViewModels.Configuration
+{
+    internal abstract class BasePropertyPageModel
+    {
+        private readonly RedisConnection _connection;
+        private readonly Dictionary<string, ConfigKeyValue> _config;
+        private readonly Dictionary<string, IPropertyModel> _mapping;
+
+        protected BasePropertyPageModel(RedisConnection connection, Dictionary<string, ConfigKeyValue> config)
+        {
+            _connection = connection;
+            _config = config;
+            _mapping = new Dictionary<string, IPropertyModel>();
+        }
+
+        protected StringPropertyModel MapToString(string key)
+        {
+            var model = new StringPropertyModel(_config[key]);
+            _mapping.Add(key, model);
+            return model;
+        }
+        
+        protected YesNoPropertyModel MapToYesNo(string key)
+        {
+            var model = new YesNoPropertyModel(_config[key]);
+            _mapping.Add(key, model);
+            return model;
+        }
+        
+        protected void Save()
+        {
+            foreach (var (key, value) in _mapping.Where(x => x.Value.IsPropertyChanged))
+            {
+                _connection.ConfigSet(key, value.Serialize());
+            }
+        }
+    }
+}
--- ViewModels/Configuration/NetworkPropertyPageModel.cs
+++ ViewModels/Configuration/NetworkPropertyPageModel.cs
@@ -1,16 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Quickr.Models.Configuration;
 using Quickr.Services;
 
 namespace Quickr.ViewModels.Configuration
 {
-    internal class NetworkPropertyPageModel
+    internal class NetworkPropertyPageModel: BasePropertyPageModel
     {
-        private readonly Dictionary<string, IPropertyModel> _mapping;
-        private readonly Dictionary<string, ConfigKeyValue> _config;
-        private readonly RedisConnection _connection;
-
         public StringPropertyModel Bind { get; set; }
         public StringPropertyModel Port { get; set; }
         public YesNoPropertyModel ProtectedMode { get; set; }
@@ -20,12 +15,9 @@ namespace Quickr.ViewModels.Configuration
         public StringPropertyModel Timeout { get; set; }
         public StringPropertyModel TcpKeepalive { get; set; }
 
-        public NetworkPropertyPageModel(RedisConnection connection, Dictionary<string, ConfigKeyValue> config)
+        public NetworkPropertyPageModel(RedisConnection connection, Dictionary<string, ConfigKeyValue> config): 
+            base(connection, config)
         {
-            _connection = connection;
-            _config = config;
-            _mapping = new Dictionary<string, IPropertyModel>();
-
             Bind = MapToString("bind");
             Port = MapToString("port");
             ProtectedMode = MapToYesNo("protected-mode");
@@ -34,28 +26,6 @@ namespace Quickr.ViewModels.Configuration
             Unixsocketperm = MapToString("unixsocketperm");
             Timeout = MapToString("timeout");
             TcpKeepalive= MapToString("tcp-keepalive");
-        }
-
-        private StringPropertyModel MapToString(string key)
-        {
-            var model = new StringPropertyModel(_config[key]);
-            _mapping.Add(key, model);
-            return model;
-        }
-        
-        private YesNoPropertyModel MapToYesNo(string key)
-        {
-            var model = new YesNoPropertyModel(_config[key]);
-            _mapping.Add(key, model);
-            return model;
-        }
-
-        public void Save()
-        {
-            foreach (var (key, value) in _mapping.Where(x => x.Value.IsPropertyChanged))
-            {
-                _connection.ConfigSet(key, value.Serialize());
-            }
         }
     }
 }
