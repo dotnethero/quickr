625bf71c1eab69f8e59559c9d0cb0e57ae5fecc4
Andrii Horbokon <andrii.horbokon@gmail.com>
2020-04-08 21:12:57 +0300

    Read-only styles and configs

--- ViewModels/Configuration/Pages/AdvancedConfigPropertyPageModel.cs
+++ ViewModels/Configuration/Pages/AdvancedConfigPropertyPageModel.cs
@@ -16,14 +16,14 @@ namespace Quickr.ViewModels.Configuration
         public StringPropertyModel HllSparseMaxBytes { get; set; }
         public StringPropertyModel StreamNodeMaxBytes { get; set; }
         public StringPropertyModel StreamNodeMaxEntries { get; set; }
-        public StringPropertyModel Activerehashing { get; set; }
+        public YesNoPropertyModel ActiveRehashing { get; set; }
         public StringPropertyModel ClientOutputBufferLimit { get; set; }
         public StringPropertyModel ClientQueryBufferLimit { get; set; }
         public StringPropertyModel ProtoMaxBulkLen { get; set; }
         public StringPropertyModel Hz { get; set; }
-        public StringPropertyModel DynamicHz { get; set; }
-        public StringPropertyModel AofRewriteIncrementalFsync { get; set; }
-        public StringPropertyModel RdbSaveIncrementalFsync { get; set; }
+        public YesNoPropertyModel DynamicHz { get; set; }
+        public YesNoPropertyModel AofRewriteIncrementalFsync { get; set; }
+        public YesNoPropertyModel RdbSaveIncrementalFsync { get; set; }
         public StringPropertyModel LfuLogFactor { get; set; }
         public StringPropertyModel LfuDecayTime { get; set; }
 
@@ -40,14 +40,14 @@ namespace Quickr.ViewModels.Configuration
             HllSparseMaxBytes = MapToString("hll-sparse-max-bytes");
             StreamNodeMaxBytes = MapToString("stream-node-max-bytes");
             StreamNodeMaxEntries = MapToString("stream-node-max-entries");
-            Activerehashing = MapToString("activerehashing");
+            ActiveRehashing = MapToYesNo("activerehashing");
             ClientOutputBufferLimit = MapToString("client-output-buffer-limit");
             ClientQueryBufferLimit = MapToString("client-query-buffer-limit");
             ProtoMaxBulkLen = MapToString("proto-max-bulk-len");
             Hz = MapToString("hz");
-            DynamicHz = MapToString("dynamic-hz");
-            AofRewriteIncrementalFsync = MapToString("aof-rewrite-incremental-fsync");
-            RdbSaveIncrementalFsync = MapToString("rdb-save-incremental-fsync");
+            DynamicHz = MapToYesNo("dynamic-hz");
+            AofRewriteIncrementalFsync = MapToYesNo("aof-rewrite-incremental-fsync");
+            RdbSaveIncrementalFsync = MapToYesNo("rdb-save-incremental-fsync");
             LfuLogFactor = MapToString("lfu-log-factor");
             LfuDecayTime = MapToString("lfu-decay-time");
         }
