5ee3cc3d6fe6c4787f748b1d34b1b553ddb701aa
Andrii Horbokon <andrii.horbokon@gmail.com>
2020-06-22 20:59:30 +0300

    Handle cancel button

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
+using Quickr.Models.Keys;
 using Quickr.Utils;
 using Quickr.ViewModels;
 
@@ -22,52 +25,64 @@ namespace Quickr.Views
 
         private async void OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
-            if (!ViewModel.IsUnsaved)
-            {
-                await ViewModel.Select(e.NewValue);
-            }
-            else
-            {
-                var result = MessageBox.Show(
-                    this,
-                    "Key has unsaved changes! Do you want to save them?",
-                    "Unsaved changes!",
-                    MessageBoxButton.YesNoCancel,
-                    MessageBoxImage.Warning);
-
-                switch (result)
-                {
-                    case MessageBoxResult.Yes:
-                        await ViewModel.Save();
-                        await ViewModel.Select(e.NewValue);
-                        break;
-                    case MessageBoxResult.No:
-                        await ViewModel.Select(e.NewValue);
-                        break;
-                    case MessageBoxResult.Cancel:
-                        // TODO: Cancel select
-                        break;
-                }
-            }
         }
 
-        private void BeforeRightClick(object sender, MouseButtonEventArgs e)
+        private async Task<bool> Select(BaseEntry entry)
+        {
+            if (!ViewModel.IsUnsaved)
+            {
+                await ViewModel.Select(entry);
+                return true;
+            }
+
+            var result = MessageBox.Show(
+                this,
+                "Key has unsaved changes! Do you want to save them?",
+                "Unsaved changes!",
+                MessageBoxButton.YesNoCancel,
+                MessageBoxImage.Warning);
+
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    await ViewModel.Save();
+                    await ViewModel.Select(entry);
+                    return true;
+
+                case MessageBoxResult.No:
+                    await ViewModel.Select(entry);
+                    return true;
+            }
+
+            return false;
+        }
+
+        private async void BeforeClick(object sender, MouseButtonEventArgs e)
         {
             if (sender is TreeView && e.OriginalSource is DependencyObject el)
             {
-                var item = FindAncestor<TreeViewItem>(el);
-                if (item != null)
+                var item = FindAncestor<TreeViewItem, ToggleButton>(el);
+                if (item != null && item.DataContext is BaseEntry entry)
                 {
-                    item.IsSelected = true;
+                    var proceed = await Select(entry);
+                    if (proceed)
+                    {
+                        item.IsSelected = true;
+                    }
+                    else
+                    {
+                        e.Handled = true;
+                    }
                 }
             }
         }
 
-        private static T FindAncestor<T>(DependencyObject el) where T: DependencyObject
+        private static T FindAncestor<T, TExcluded>(DependencyObject el) where T: DependencyObject
         {
             while (el != null)
             {
                 if (el is T ancestor) return ancestor;
+                if (el is TExcluded _) return null;
                 el = VisualTreeHelper.GetParent(el);
             }
             return null;
